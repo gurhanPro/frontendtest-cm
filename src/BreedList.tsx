dadc5d5e0aa13bc01a12f893bc720a29539982fe
gurhanpro <ziyaadyare@gmail.com>
2026-04-16 12:07:23 +0200

    format code with prettier

--- src/BreedList.tsx
+++ src/BreedList.tsx
@@ -7,9 +7,9 @@ export default function BreedList() {
   const [breeds, setBreeds] = useState<BreedType[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [filterInput, setFilterInput ] = useState('')
-  const [selectedBreed, setSelectedBreed] = useState<BreedType | null>(null)
-  const [debouncedFitler, setDebouncedFilter]= useState('')
+  const [filterInput, setFilterInput] = useState("");
+  const [selectedBreed, setSelectedBreed] = useState<BreedType | null>(null);
+  const [debouncedFitler, setDebouncedFilter] = useState("");
 
   useEffect(() => {
     const fetchBreeds = async () => {
@@ -32,47 +32,63 @@ export default function BreedList() {
     fetchBreeds();
   }, []);
 
-  useEffect(()=> {
-    const timer = setTimeout(()=> { setDebouncedFilter(filterInput)}, 300)
-    return () => clearTimeout(timer)
-  },[filterInput])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedFilter(filterInput);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [filterInput]);
 
-  const filtered = useMemo(()=> {
-    return debouncedFitler ? breeds.filter(breed => breed.name.toLowerCase().includes(debouncedFitler.toLowerCase())) : breeds
-  },[breeds, debouncedFitler])
+  const filtered = useMemo(() => {
+    return debouncedFitler
+      ? breeds.filter((breed) =>
+          breed.name.toLowerCase().includes(debouncedFitler.toLowerCase()),
+        )
+      : breeds;
+  }, [breeds, debouncedFitler]);
 
   return (
     <>
       {loading ? <p>Loading dog breeds ...</p> : null}
       {error ? <p>error loading</p> : null}
-      <div style={{display: 'flex', gap: '50px'}}>
-      <div>
-        <h1>BreedLists</h1>
-        <input
-          value={filterInput}
-          onChange={(event) => setFilterInput(event.target.value)}
-          placeholder="type a bread name"
-        />
-              {filtered.length > 0 ? (
+      <div style={{ display: "flex", gap: "50px" }}>
         <div>
-          <div style={{ display: "flex", gap: "100px" }}>
-            <ul>
-              {filtered.map((breed) => (
-                <li key={breed.name} onClick={()=> setSelectedBreed(breed)} style={{background: selectedBreed?.name === breed.name ? 'orange' : 'none'}}>{breed.name}</li>
-              ))}
-            </ul>
-          </div>
+          <h1>BreedLists</h1>
+          <input
+            value={filterInput}
+            onChange={(event) => setFilterInput(event.target.value)}
+            placeholder="type a bread name"
+          />
+          {filtered.length > 0 ? (
+            <div>
+              <div style={{ display: "flex", gap: "100px" }}>
+                <ul>
+                  {filtered.map((breed) => (
+                    <li
+                      key={breed.name}
+                      onClick={() => setSelectedBreed(breed)}
+                      style={{
+                        background:
+                          selectedBreed?.name === breed.name
+                            ? "orange"
+                            : "none",
+                      }}
+                    >
+                      {breed.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <p>no breeds to match your filter input</p>
+          )}
         </div>
-      ) : (
-        <p>no breeds to match your filter input</p>
-      )}
-      </div>
 
-      <div>
-        <h2>selected breed: {selectedBreed?.name}</h2>
+        <div>
+          <h2>selected breed: {selectedBreed?.name}</h2>
+        </div>
       </div>
-      </div>
-
     </>
   );
 }
