46b57939d8f310a93b3a7ad74077e598cc25f639
gurhanpro <ziyaadyare@gmail.com>
2026-04-16 14:14:22 +0200

    style input compeont

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,3 +1,4 @@
+import styles from "./SearchInput.module.scss";
 
 export default function SearchInput({value, onChange} : { value:string, onChange: (v: string)=>void }){
   return (
@@ -5,6 +6,7 @@ export default function SearchInput({value, onChange} : { value:string, onChange
       value={value}
       onChange={(e)=> onChange(e.target.value)}
       placeholder="type a bread name"
+      className={styles.input}
     />
   )
 }
