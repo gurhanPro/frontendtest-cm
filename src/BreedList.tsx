59e7c8d2d376670e40db68f8d8f99097c88f4120
gurhanpro <ziyaadyare@gmail.com>
2026-04-16 11:45:11 +0200

    add a filter input to filter by breed name

--- src/BreedList.tsx
+++ src/BreedList.tsx
@@ -7,6 +7,7 @@ export default function BreedList() {
   const [breeds, setBreeds] = useState<BreedType[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [filterInput, setFilterInput ] = useState('')
 
   useEffect(() => {
     const fetchBreeds = async () => {
@@ -29,16 +30,23 @@ export default function BreedList() {
     fetchBreeds();
   }, []);
 
+  const filtered = filterInput ? breeds.filter(breed => breed.name.toLowerCase().includes(filterInput.toLowerCase())) : breeds
+
   return (
     <>
       {loading ? <p>Loading dog breeds ...</p> : null}
       {error ? <p>error loading</p> : null}
-      {breeds.length > 0 && (
+      {filtered.length > 0 && (
         <div>
           <h1>BreedLists</h1>
+          <input
+            value={filterInput}
+            onChange={(event)=> setFilterInput(event.target.value)}
+            placeholder="type a bread name"
+          />
           <div style={{ display: "flex", gap: "100px" }}>
             <ul>
-              {breeds.map((breed) => (
+              {filtered.map((breed) => (
                 <li key={breed.name}>{breed.name}</li>
               ))}
             </ul>
